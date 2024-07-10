0a513a75fd111ccd09de3af6db305db55b91b096
vahidmohammadi2310 <vahid73.mohammadi@yahoo.com>
2024-07-10 15:14:56 +0330

    add select fields to index and show BaseFetchInterface methods

--- app/Repositories/Interfaces/BaseInterfaces/BaseFetchInterface.php
+++ app/Repositories/Interfaces/BaseInterfaces/BaseFetchInterface.php
@@ -6,14 +6,16 @@ interface BaseFetchInterface
 {
     /**
      * list of resources
+     * @param array $select
      * @return mixed
      */
-    public function index();
+    public function index(array $select);
 
     /**
      * find details op resource according to resource id
      * @param $resourceId
+     * @param array $select
      * @return mixed
      */
-    public function show($resourceId);
+    public function show($resourceId, array $select);
 }
